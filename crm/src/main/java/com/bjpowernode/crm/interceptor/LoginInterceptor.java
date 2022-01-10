0a4e67d312b3015db2087faa254342bc394b8ca3
liuxiaolong <liuxiaolong@bjpowernode.com>
2022-01-10 17:00:46 +0800

    提交代码到本地仓库，架构搭建

---
+++ crm/src/main/java/com/bjpowernode/crm/interceptor/LoginInterceptor.java
@@ -0,0 +1,30 @@
+package com.bjpowernode.crm.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Description TODO
+ * @Date 2022/1/10 16:44
+ * @Version 1.0
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    //控制器访问前的拦截方法
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        return false;
+    }
+    //控制器访问后的拦截方法
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+    //页面加载完成前的回调+方法
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
